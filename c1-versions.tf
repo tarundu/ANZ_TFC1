e0259db5eb915d05973757721a6f5ab62f4f17f7
Lab User <lab-user@ip-172-31-104-103.ap-south-2.compute.internal>
2024-06-28 11:28:16 +0530

    test

--- c1-versions.tf
+++ c1-versions.tf
@@ -4,7 +4,7 @@ terraform {
     organization = "tarunterraform"
 
     workspaces {
-      name = "TestWorkspace1"
+      name = "ANZ_TFC1"
     }
   }
   required_version = ">= 1.6" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
